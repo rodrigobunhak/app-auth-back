2034d629dff64d774c0f65e62f2105774fab58dc
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2025-01-04 11:40:12 -0300

    Add in memory repository implementation

---
+++ src/infrastructure/repositories/in-memory-user.repository.ts
@@ -0,0 +1,33 @@
+import { User } from "@/domain/entities/user.entity";
+import { UserRepository } from "@/domain/repositories/user.repository";
+import { injectable } from "inversify";
+
+@injectable()
+export class InMemoryUserRepository implements UserRepository {
+  private users: User[] = [];
+
+  async save(user: User): Promise<boolean> {
+    const existingUser = this.users.find(u => u.email.value() === user.email.value());
+    if (existingUser) {
+      Object.assign(existingUser, user);
+    } else {
+      this.users.push(user);
+    }
+    return true;
+  }
+  
+  async findByEmail(email: string): Promise<User> {
+    const user = this.users.find(u => u.email.value() === email);
+    if (!user) {
+      throw new Error(`User with email ${email} not found`);
+    }
+    return user;
+  }
+
+  async findAll(page: number, limit: number): Promise<{ users: User[]; total: number; }> {
+    const start = (page - 1) * limit;
+    const end = start + limit;
+    const paginatedUsers = this.users.slice(start, end);
+    return { users: paginatedUsers, total: this.users.length };
+  }
+}
