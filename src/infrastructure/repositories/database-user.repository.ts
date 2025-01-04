551baa795c8f1dd5d72dcdddd352ee92de3aed6d
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2025-01-04 11:40:29 -0300

    Add database repository

---
+++ src/infrastructure/repositories/database-user.repository.ts
@@ -0,0 +1,19 @@
+import { User } from "@/domain/entities/user.entity";
+import { UserRepository } from "@/domain/repositories/user.repository";
+import { injectable } from "inversify";
+
+@injectable()
+export class DatabaseUserRepository implements UserRepository {
+  
+  async save(user: User): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
+  
+  async findByEmail(email: string): Promise<User> {
+    throw new Error("Method not implemented.");
+  }
+
+  async findAll(page: number, limit: number): Promise<{ users: User[]; total: number; }> {
+    throw new Error("Method not implemented.");
+  }
+}
